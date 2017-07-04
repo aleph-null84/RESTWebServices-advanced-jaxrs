5a96fa014b8153a16c2b67668196aa2671793872
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-04 20:58:30 +0300

    lesson #07

---
+++ src/main/java/com/yuriy/traning/MyResource3.java
@@ -0,0 +1,25 @@
+package com.yuriy.traning;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("{pathParam}/test")
+public class MyResource3 {
+
+	@PathParam("pathParam")
+	private String pathParamExample;
+	
+	@QueryParam("query")
+	private String queryParamExample;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String testMethod() {
+		return "It works! Path param used " + pathParamExample + " and Query param used " + queryParamExample;
+	}
+
+}
