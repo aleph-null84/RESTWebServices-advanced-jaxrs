444e4763add17b896f4bdeb5819c417f00975c50
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-05 17:19:32 +0300

    lesson #11

--- src/main/java/com/yuriy/traning/example04/DateResource.java
+++ src/main/java/com/yuriy/traning/example04/DateResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("date/{dateString}")
+@Path("test04/{dateString}")
 public class DateResource {
 
 	@GET
