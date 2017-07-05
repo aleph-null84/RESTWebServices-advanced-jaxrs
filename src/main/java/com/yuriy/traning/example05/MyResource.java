d9ea742c3b6c351f375bf018d8359f3a17523e2c
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-05 16:43:24 +0300

    lesson 11

--- src/main/java/com/yuriy/traning/example05/MyResource.java
+++ src/main/java/com/yuriy/traning/example05/MyResource.java
@@ -3,14 +3,12 @@ package com.yuriy.traning.example05;
 import java.util.Calendar;
 import java.util.Date;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("test05")
-@Singleton
 public class MyResource {
 
 	@GET
